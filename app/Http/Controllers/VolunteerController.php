a95527881a9803c57b1578657dbbd3e204ebe0cf
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-12-29 05:58:12 +0700

    chore: Change chart data to year

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -31,7 +31,7 @@ class VolunteerController extends Controller
         }
 
         $currentDate = Carbon::now();
-        $fourMonthsAgo = Carbon::parse(Carbon::now()->format('Y-m'))->subMonths(9);
+        $fourMonthsAgo = Carbon::parse(Carbon::now()->format('Y-m'))->subMonths(11);
 
         $donations = Donation::where('charity', 0)->whereBetween('take', [$fourMonthsAgo, $currentDate])->with(['foods', 'heroes'])->get();
         $groupedData = $donations->groupBy(function ($donation) {
