85fcae53f811d4d98729d30b1e92eb358680afb5
d-arsya <kamaluddin.arsyad17@gmail.com>
2026-01-15 16:59:46 +0700

    fix: Disable some bot features

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -24,12 +24,12 @@ class BotController extends Controller
         $data = json_decode($json, true);
         $sender = $data['sender'];
         $message = $data['message'];
-        $media = $data['media'];
-        if ($media) {
-            dispatch(function () use ($data) {
-                $this->handleMedia($data['media']);
-            });
-        }
+        // $media = $data['media'];
+        // if ($media) {
+        //     dispatch(function () use ($data) {
+        //         $this->handleMedia($data['media']);
+        //     });
+        // }
         // $group = explode(',', AppConfiguration::getGroupCode());
         if (str_ends_with($sender, '@g.us')) {
             if ($message == '@BOT donasi hari ini') {
@@ -45,7 +45,8 @@ class BotController extends Controller
             } elseif ($message == '@BOT ingatkan hero hari ini') {
                 $this->reminderToday($sender);
             } elseif ($message == '@all') {
-                $this->mentionAll($data['sender']);
+                $this->send($sender, 'Maaf sedang perbaikan');
+                // $this->mentionAll($data['sender']);
             } elseif (str_starts_with($message, '@BOT pemkot')) {
                 $this->sendToPemkot($data);
             } elseif (str_starts_with($message, '@BOT ingatkan hero yang belum')) {
@@ -78,49 +79,48 @@ class BotController extends Controller
         if ($hero) {
             $this->getReplyFromHeroes($hero, $text);
         } elseif ($volunteer) {
+
             $this->getReplyFromVolunteer($volunteer, $text, $media);
         }
         return true;
     }
 
-    public static function sendForPublic($target, $message, $from = 'FIRST')
-    {
-        Http::post(AppConfiguration::getWhatsAppEndpoint() . '/send', [
-            'target' => $target,
-            'message' => $message,
-        ]);
-    }
     // public static function sendForPublic($target, $message, $from = 'FIRST')
     // {
-    //     $curl = curl_init();
-    //     if (!str_ends_with($target, '@g.us')) {
-    //         $from = 'SECOND';
-    //     }
-    //     $token = AppConfiguration::where('key', "FONNTE_$from")->first()->value;
-
-    //     curl_setopt_array($curl, [
-    //         CURLOPT_URL => 'https://api.fonnte.com/send',
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_ENCODING => '',
-    //         CURLOPT_MAXREDIRS => 10,
-    //         CURLOPT_TIMEOUT => 0,
-    //         CURLOPT_FOLLOWLOCATION => true,
-    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //         CURLOPT_CUSTOMREQUEST => 'POST',
-    //         CURLOPT_POSTFIELDS => [
-    //             'target' => $target,
-    //             'message' => $message,
-    //             'schedule' => 0,
-    //             'typing' => false,
-    //             'delay' => '2',
-    //             'countryCode' => '62',
-    //         ],
-    //         CURLOPT_HTTPHEADER => [
-    //             'Authorization: ' . $token,
-    //         ],
+    //     Http::post(AppConfiguration::getWhatsAppEndpoint() . '/send', [
+    //         'target' => $target,
+    //         'message' => $message,
     //     ]);
-
-    //     curl_exec($curl);
-    //     curl_close($curl);
     // }
+    public static function sendForPublic($target, $message, $from = 'FIRST')
+    {
+        $curl = curl_init();
+
+        $token = AppConfiguration::where('key', "FONNTE_FIRST")->first()->value;
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://api.fonnte.com/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => [
+                'target' => $target,
+                'message' => $message,
+                'schedule' => 0,
+                'typing' => false,
+                'delay' => '2',
+                'countryCode' => '62',
+            ],
+            CURLOPT_HTTPHEADER => [
+                'Authorization: ' . $token,
+            ],
+        ]);
+
+        curl_exec($curl);
+        curl_close($curl);
+    }
 }
