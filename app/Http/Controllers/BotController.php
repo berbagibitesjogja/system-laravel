807e10d746c213e68eda6ad772d483700c991a91
d-arsya <kamaluddin.arsyad17@gmail.com>
2026-01-19 07:23:09 +0700

    fix: Change whatsapp services

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -85,42 +85,50 @@ class BotController extends Controller
         return true;
     }
 
+    public static function sendForPublic($target, $message, $media = 'FIRST')
+    {
+        if (in_array($media, ['FIRST', 'SECOND'])) {
+            $media = null;
+        }
+        $token = AppConfiguration::where('key', "FONNTE_FIRST")->first()->value;
+        Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])
+        ->post(AppConfiguration::getWhatsAppEndpoint() . '/send', [
+            'target'  => $target,
+            'message' => $message,
+            'media'   => $media,
+        ]);
+    }
     // public static function sendForPublic($target, $message, $from = 'FIRST')
     // {
-    //     Http::post(AppConfiguration::getWhatsAppEndpoint() . '/send', [
-    //         'target' => $target,
-    //         'message' => $message,
+    //     $curl = curl_init();
+
+    //     $token = AppConfiguration::where('key', "FONNTE_FIRST")->first()->value;
+
+    //     curl_setopt_array($curl, [
+    //         CURLOPT_URL => 'https://api.fonnte.com/send',
+    //         CURLOPT_RETURNTRANSFER => true,
+    //         CURLOPT_ENCODING => '',
+    //         CURLOPT_MAXREDIRS => 10,
+    //         CURLOPT_TIMEOUT => 0,
+    //         CURLOPT_FOLLOWLOCATION => true,
+    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //         CURLOPT_CUSTOMREQUEST => 'POST',
+    //         CURLOPT_POSTFIELDS => [
+    //             'target' => $target,
+    //             'message' => $message,
+    //             'schedule' => 0,
+    //             'typing' => false,
+    //             'delay' => '2',
+    //             'countryCode' => '62',
+    //         ],
+    //         CURLOPT_HTTPHEADER => [
+    //             'Authorization: ' . $token,
+    //         ],
     //     ]);
+
+    //     curl_exec($curl);
+    //     curl_close($curl);
     // }
-    public static function sendForPublic($target, $message, $from = 'FIRST')
-    {
-        $curl = curl_init();
-
-        $token = AppConfiguration::where('key', "FONNTE_FIRST")->first()->value;
-
-        curl_setopt_array($curl, [
-            CURLOPT_URL => 'https://api.fonnte.com/send',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => [
-                'target' => $target,
-                'message' => $message,
-                'schedule' => 0,
-                'typing' => false,
-                'delay' => '2',
-                'countryCode' => '62',
-            ],
-            CURLOPT_HTTPHEADER => [
-                'Authorization: ' . $token,
-            ],
-        ]);
-
-        curl_exec($curl);
-        curl_close($curl);
-    }
 }
